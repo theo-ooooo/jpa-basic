d9fdfa44d4b569f556832c52490f1b112c470689
theo-ooooo <kkw.theo@gmail.com>
2025-02-06 15:07:18 +0900

    Mapped Superclass - 매핑 정보 상속

--- src/main/java/helloJpa/JpaMain.java
+++ src/main/java/helloJpa/JpaMain.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class JpaMain {
@@ -20,21 +21,12 @@ public class JpaMain {
 
         try {
 
-            Movie movie = new Movie();
+            Member member = new Member();
+            member.setCreatedBy("kang");
+            member.setCreatedDate(LocalDateTime.now());
+            member.setName("kyungwon");
 
-            movie.setActor("A");
-            movie.setDirector("BBBB");
-            movie.setName("바람과 함께살아지다");
-            movie.setPrice(100000);
-
-            em.persist(movie);
-
-            em.flush();
-            em.clear();
-
-            Item findMovie = em.find(Item.class, movie.getId());
-
-            System.out.println("findMovie = " + findMovie.getName());
+            em.persist(member);
 
 
             tx.commit();
